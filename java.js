45971cd0709437a6dee40878e5cb287234bb1854
Alawode Muneef Opeyemi <muneefopeyemi@gmail.com>
2023-05-15 17:12:43 +0100

    updated code

---
+++ java.js
@@ -0,0 +1,17 @@
+// // Select the body element
+// const body = document.querySelector('body');
+
+// // Add event listener for window resize
+// window.addEventListener('resize', () => {
+//   // Get the viewport width
+//   const viewportWidth = window.innerWidth;
+
+//   // Change the background color based on viewport width
+//   if (viewportWidth < 576) {
+//     body.style.backgroundColor = 'lightblue';
+//   } else if (viewportWidth >= 576 && viewportWidth < 992) {
+//     body.style.backgroundColor = 'lightgreen';
+//   } else {
+//     body.style.backgroundColor = 'lightgrey';
+//   }
+// });
