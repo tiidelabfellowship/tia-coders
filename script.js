f08d316f6779d5c4acc14997401f329786557d84
Ayodeji Abolaji <abolajiayodeji93@gmail.com>
2023-05-15 10:49:58 +0100

    I developed the Navigation bar and the hero section for bookmyevent

---
+++ script.js
@@ -0,0 +1,8 @@
+function classToggle() {
+    const navs = document.querySelectorAll('.))
+    
+    navs.forEach(nav => nav.classList.toggle('));
+  }
+  
+  document.querySelector('.Navbar__Link-toggle')
+    .addEventListener('click', classToggle);
